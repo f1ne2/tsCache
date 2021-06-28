4ee920e2802d8f48393e45ba5863df9d0432f490
f1ne2 <alextomilovski@gmail.com>
2021-06-28 15:20:56 +0300

    .js removed

--- User.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.User = void 0;
-var Decoder = require("io-ts/Decoder");
-// eslint-disable-next-line no-redeclare
-exports.User = Decoder.lazy("User", function () { return Decoder.struct({
-    login: Decoder.string,
-    id: Decoder.number
-}); });
